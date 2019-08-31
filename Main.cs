e78834765175decbff4543925cacf122c24aa91b
Benjamin McLean <mclean.ben@gmail.com>
2019-08-31 17:59:39 -0500

    added play button

--- Main.cs
+++ Main.cs
@@ -9,20 +9,63 @@ using static OPL.Imf;
 
 public class Main : Control
 {
+    ImfPlayer ImfPlayer;
+
     public override void _Ready()
     {
-        ImfPlayer imfPlayer = new ImfPlayer()
+        ImfPlayer ImfPlayer = new ImfPlayer()
         {
             Opl = new DosBoxOPL(OplType.Opl3),
             Song = ReadImf("WONDERIN_MUS.imf"),
             AudioStreamPlayer = new AudioStreamPlayer()
         };
-        AddChild(imfPlayer);
-        AddChild(imfPlayer.AudioStreamPlayer);
+        AddChild(ImfPlayer);
+        AddChild(ImfPlayer.AudioStreamPlayer);
+
+        Button button = new PlayButton
+        {
+            Text = "Stop",
+            AudioStreamPlayer = ImfPlayer.AudioStreamPlayer
+        };
+        AddChild(button);
+
+        SongStep songStep = new SongStep
+        {
+            ImfPlayer = ImfPlayer
+        };
+        AddChild(songStep);
     }
 
     //public override void _Process(float delta)
     //{
 
     //}
+
+    public class SongStep : Label
+    {
+        public ImfPlayer ImfPlayer { get; set; }
+
+        public override void _Process(float delta)
+        {
+            base._Process(delta);
+            Text = ImfPlayer.CurrentPacket.ToString();
+        }
+    }
+
+    public class PlayButton : Button
+    {
+        public AudioStreamPlayer AudioStreamPlayer { get; set; }
+
+        public override void _GuiInput(InputEvent @event)
+        {
+            base._GuiInput(@event);
+            if (@event.IsPressed())
+            {
+                if (AudioStreamPlayer.Playing = !AudioStreamPlayer.Playing)
+                    SetText("Stop");
+                else
+                    SetText("Play");
+            }
+        }
+    }
 }
