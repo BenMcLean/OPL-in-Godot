11b7a6d55adf9fa18c3dd67c371195aa0e41bb72
Benjamin McLean <mclean.ben@gmail.com>
2019-09-01 16:58:14 -0500

    Replaced references to FileStream with Stream

--- OPL/Imf.cs
+++ OPL/Imf.cs
@@ -27,44 +27,41 @@ namespace OPL
             public ushort Delay { get; set; }
         }
 
-        public static ushort ReadWord(this FileStream file)
+        public static ushort ReadWord(this Stream stream)
         {
-            return (ushort)(file.ReadByte() + (file.ReadByte() << 8));
+            return (ushort)(stream.ReadByte() + (stream.ReadByte() << 8));
         }
 
-        public static ImfPacket ReadImfPacket(this FileStream file)
+        public static ImfPacket ReadImfPacket(this Stream stream)
         {
             return new ImfPacket()
             {
-                Register = (byte)file.ReadByte(),
-                Data = (byte)file.ReadByte(),
-                Delay = file.ReadWord()
+                Register = (byte)stream.ReadByte(),
+                Data = (byte)stream.ReadByte(),
+                Delay = stream.ReadWord()
             };
         }
 
         /// <summary>
         /// Reading in IMF files based on http://www.shikadi.net/moddingwiki/IMF_Format
         /// </summary>
-        public static ImfPacket[] ReadImf(string filename)
+        public static ImfPacket[] ReadImf(Stream stream)
         {
             ImfPacket[] imf;
-            using (FileStream file = new FileStream(filename, FileMode.Open))
-            {
-                ushort length = (ushort)(file.ReadWord() / 4); // Length is provided in number of bytes. Divide by 4 to get the number of 4 byte packets.
-                if (length == 0)
-                { // Type-0 format
-                    file.Seek(0, 0);
-                    List<ImfPacket> list = new List<ImfPacket>();
-                    while (file.Position < file.Length)
-                        list.Add(file.ReadImfPacket());
-                    imf = list.ToArray();
-                }
-                else
-                { // Type-1 format
-                    imf = new ImfPacket[length];
-                    for (uint i = 0; i < imf.Length; i++)
-                        imf[i] = file.ReadImfPacket();
-                }
+            ushort length = (ushort)(stream.ReadWord() / 4); // Length is provided in number of bytes. Divide by 4 to get the number of 4 byte packets.
+            if (length == 0)
+            { // Type-0 format
+                stream.Seek(0, 0);
+                List<ImfPacket> list = new List<ImfPacket>();
+                while (stream.Position < stream.Length)
+                    list.Add(stream.ReadImfPacket());
+                imf = list.ToArray();
+            }
+            else
+            { // Type-1 format
+                imf = new ImfPacket[length];
+                for (uint i = 0; i < imf.Length; i++)
+                    imf[i] = stream.ReadImfPacket();
             }
             return imf;
         }
